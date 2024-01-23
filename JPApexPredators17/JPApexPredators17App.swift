5787e4b6a768d04703547a8a0edae0033541abe8
Angel Martinez <angel.m.97@hotmail.com>
2024-01-23 13:09:27 -0600

    Initial Commit

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by Angel Martínez Escobar on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
