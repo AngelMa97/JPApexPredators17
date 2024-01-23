be0c25d638e873deb12f8b4900b143f9a2c23f04
Angel Martinez <angel.m.97@hotmail.com>
2024-01-23 14:59:28 -0600

    Predator list view added Predator model has been modified

--- JPApexPredators17/ContentView.swift
+++ JPApexPredators17/ContentView.swift
@@ -8,15 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    let predatorsRepo = Predators()
+    
     var body: some View {
-        VStack {
-            let predatorsRepo = Predators()
-            
-            LazyVStack {
+        List(predatorsRepo.apexPredators) { predator in
+            HStack {
+                Image(predator.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .shadow(color: .white, radius: 1)
                 
+                VStack(alignment: .leading) {
+                    Text(predator.name)
+                        .fontWeight(.bold)
+                    
+                    Text(predator.type.rawValue.capitalized)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 13)
+                        .padding(.vertical, 5)
+                        .background(predator.type.background)
+                        .clipShape(.capsule)
+                }
             }
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
 }
 
