941f99fc02c9f8fd30eb20a7f7fd10c7e1bf7e4c
Angel Martinez <angel.m.97@hotmail.com>
2024-01-23 14:28:42 -0600

    Add predators repository

---
+++ JPApexPredators17/Predators.swift
@@ -0,0 +1,30 @@
+//
+//  Predators.swift
+//  JPApexPredators17
+//
+//  Created by Angel Martínez Escobar on 23/01/24.
+//
+
+import Foundation
+
+class Predators {
+    var apexPredators: [ApexPredator] = []
+    
+    init() {
+        decodeApexPredatorData()
+    }
+    
+    func decodeApexPredatorData() {
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                apexPredators = try decoder.decode([ApexPredator].self, from: data)
+            } catch {
+                print("Error decoding JSON data: \(error)")
+            }
+        }
+    }
+    
+}
